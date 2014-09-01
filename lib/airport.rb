64bff4ec10a0d8edab423e2c42324c93eef8b030
javier <js@silveriofunk.com>
2014-09-01 18:47:29 +0100

    all tests passed. grand finale missing

--- lib/airport.rb
+++ lib/airport.rb
@@ -25,10 +25,13 @@ DEFAULT_CAPACITY = 6
   def land(plane)
   	raise 'Airport full!' if full?
   	@planes << plane unless stormy?
+  	raise "Unable to land due to bad weather!" if stormy? == true
+
   end
 
   def take_off(plane)
   	@planes.delete(plane) unless stormy?
+  	raise "Unable to take off due to bad weather!" if stormy? == true
   end
 
 
